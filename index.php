513643cffa70b3ed62fe8a4423e28c30193a4151
Vipul Rathod <vipsravs787@gmail.com>
2023-12-24 11:39:01 +0530

    Added a table to view the notes added by the user

--- index.php
+++ index.php
@@ -44,6 +44,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>iNotes - Notes taking made easy</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    
+    <link rel="stylesheet" href="//cdn.datatables.net/1.13.7/css/jquery.dataTables.min.css">
+
   </head>
   <body>
 
@@ -87,8 +90,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
             <button type="submit" class="btn btn-primary">Add Note</button>
         </form>
     </div>
-    <div class="container">
-      <table class="table">
+    <div class="container my-4">
+      <table class="table" id="myTable">
         <thead>
           <tr>
             <th scope="col">S.No</th>
@@ -121,7 +124,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
                 <th scope='row'>" .$row['sno'] ."</th>
                 <td>" .$row['title'] ."</td>
                 <td>" .$row['description'] ."</td>
-                <td> Actions </td>
+                <td> <button type='submit' class='btn btn-primary'>Edit</button> <button type='submit' class='btn btn-primary'>Delete</button></td>
               </tr>";
             }
         ?>
